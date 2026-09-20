b9387d4508d7119ee587464e736e3ed78cde4087
Jeffrey MacPherson <jeff_macpherson95@hotmail.com>
2026-09-19 23:10:54 -0300

    changed layout with iframe

--- game-lab/src/pages/Arcade.jsx
+++ game-lab/src/pages/Arcade.jsx
@@ -11,7 +11,7 @@ function Arcade() {
             <div className="flex flex-col md:flex-row mt-12 items-stretch">
                 <aside className="w-full md:w-1/3 ml-2">
                     <h3 className="text-accent text-xl">Games:</h3>
-                    <ul>
+                    <ul className="w-fit">
                         <li onClick={() => setShowDescription(prev => !prev)}
                         className="hover:cursor-pointer"
                         >
@@ -28,26 +28,26 @@ function Arcade() {
                             <p className="text-muted">
                                 A 2D arcade-style space shooter where you defend against incoming asteroids.
                             </p>
-                            <p>
-                                Controls:
-                                <br />
-                                <span>Move Left = A</span>
-                                <br />
-                                <span>Move Right = D</span>
-                                <br />
-                                <span>Shoot = Spacebar</span>
-                            </p>
                         </div>
                     )}
                 </aside>
                 {drawCanvas && (
-                    <div className="w-full md:w-2/3 flex justify-baseline">
+                    <div className="w-full md:w-2/3 flex flex-col md:flex-row">
                         <iframe
                             src="/milkywaydefender/game/index.html"
                             width="600"
                             height="760"
                             className="border-0"
                         />
+                        <p className="mt-2 md:mt-0 md:ml-4 text-sm text-muted">
+                            <span className="text-accent">Controls: </span>
+                            <br />
+                            Move Left: A
+                            <br />
+                            Move Right: D
+                            <br />
+                            Shoot: Spacebar
+                        </p>
                     </div>
                 )}
             </div>
