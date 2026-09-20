605429d3703cfeac7782d308c16edbb0af431730
Jeffrey MacPherson <jeff_macpherson95@hotmail.com>
2026-09-19 22:50:14 -0300

    style changes

--- game-lab/src/pages/Arcade.jsx
+++ game-lab/src/pages/Arcade.jsx
@@ -5,51 +5,52 @@ function Arcade() {
     const [drawCanvas, setDrawCanvas] = useState(false);
 
     return (
-        <div>
-            <h1 className="text-3xl text-center">Arcade</h1>
+        <div className="w-full mx-auto px-6">
+            <h1 className="text-3xl text-center underline">Arcade</h1>
 
-            {drawCanvas && (
-                <div className="max-w-md">
-                    <iframe
-                        src="/milkywaydefender/game/index.html"
-                        width="600"
-                        height="760"
-                        className="border-0"
-                    />
-                </div>
-            )}
-
-            <aside className="mt-12 ml-2">
-                Games:
-                <ul>
-                    <li onClick={() => setShowDescription(prev => !prev)}
-                    className="hover:cursor-pointer"
-                    >
-                        &gt; Milky Way Defender
-                        <button
-                            onClick={() => setDrawCanvas(prev => !prev)}
-                            className="ml-2 border rounded-md px-2 py-1 hover:bg-white hover:text-black">
-                            {drawCanvas ? "Cancel" : "Play"}
-                        </button>
-                    </li>
-                </ul>
-                {showDescription && (
-                    <div className="mt-2 max-w-md">
-                        <p className="text-muted">
-                            A 2D arcade-style space shooter where you defend against incoming asteroids.
-                        </p>
-                        <p>
-                            Controls:
-                            <br />
-                            <span>Move Left = A</span>
-                            <br />
-                            <span>Move Right = D</span>
-                            <br />
-                            <span>Shoot = Spacebar</span>
-                        </p>
+            <div className="flex flex-col md:flex-row mt-12 items-stretch">
+                <aside className="w-full md:w-1/3 ml-2">
+                    <h3 className="text-accent text-xl">Games:</h3>
+                    <ul>
+                        <li onClick={() => setShowDescription(prev => !prev)}
+                        className="hover:cursor-pointer"
+                        >
+                            &gt; Milky Way Defender
+                            <button
+                                onClick={() => setDrawCanvas(prev => !prev)}
+                                className="ml-2 border rounded-md px-2 py-1 hover:bg-accent hover:text-black">
+                                {drawCanvas ? "Cancel" : "Play"}
+                            </button>
+                        </li>
+                    </ul>
+                    {showDescription && (
+                        <div className="mt-2 max-w-md">
+                            <p className="text-muted">
+                                A 2D arcade-style space shooter where you defend against incoming asteroids.
+                            </p>
+                            <p>
+                                Controls:
+                                <br />
+                                <span>Move Left = A</span>
+                                <br />
+                                <span>Move Right = D</span>
+                                <br />
+                                <span>Shoot = Spacebar</span>
+                            </p>
+                        </div>
+                    )}
+                </aside>
+                {drawCanvas && (
+                    <div className="w-full md:w-2/3 flex justify-baseline">
+                        <iframe
+                            src="/milkywaydefender/game/index.html"
+                            width="600"
+                            height="760"
+                            className="border-0"
+                        />
                     </div>
                 )}
-            </aside>
+            </div>
         </div>
     )
 }
--- game-lab/src/pages/Projects.jsx
+++ game-lab/src/pages/Projects.jsx
@@ -1,21 +1,21 @@
 function Projects() {
     return (
         <div className="flex flex-col items-center px-6">
-            <h1 className="border-b-2 text-3xl">Projects</h1>
+            <h1 className="text-3xl underline">Projects</h1>
 
             <div className="flex flex-col justify-center items-center mt-8 p-4 border-t self-stretch">
-                <h2 className="text-xl text-accent">Worklog System</h2>
+                <h2 className="text-xl text-accent py-4">Worklog System</h2>
 
-                <div className="flex flex-row items-center">
+                <div className="flex flex-col md:flex-row items-center">
                     <img src="/worklog/auth-page.jpg"
                     alt="Log in page"
-                    className="max-w-sm px-3"
+                    className="w-full max-w-sm px-3"
                     />
                     <img src="/worklog/running.jpg"
                     alt="Components running"
-                    className="max-w-lg"
+                    className="w-full max-w-sm px-3"
                     />
-                    <p className="max-w-2xl px-4">
+                    <p className="w-full max-w-2xl px-4">
                         A full-stack time tracking app I built to keep track of how I spend my time. Users can create an account, use a fully functioning timer, log activities, search/edit/delete entries.
                         <br/>
                         <span className="text-muted">Client side: React, Vite, TypeScript, and Tailwind</span>
@@ -26,38 +26,38 @@ function Projects() {
             </div>
 
             <div className="flex flex-col justify-center items-center mt-8 p-4 border-t self-stretch">
-                <h2 className="text-xl text-accent">Milky Way Defender</h2>
+                <h2 className="text-xl text-accent py-4">Milky Way Defender</h2>
 
-                <div className="flex flex-row pt-8 items-center">
-                    <p className="max-w-2xl px-4">
+                <div className="flex flex-col md:flex-row items-center">
+                    <p className="w-full max-w-2xl px-4">
                         A 2D arcade-style space shooter. Defend against incoming asteroids with the difficulty increasing over time. Includes score and accuracy tracking, sound effects, music, and particle effects for collision.
                         <br />
                         <span className="text-muted">Tech: Java 21, LibGDX 1.12.1, LWJGL3, and Gradle</span>
                     </p>
                     <img src="/milkywaydefender/example_imgs/animations.jpg"
                     alt="Moving rocket"
-                    className="max-w-sm px-3"
+                    className="w-full max-w-sm px-3"
                     />
                     <img src="/milkywaydefender/example_imgs/particles.jpg"
                     alt="Particle from bullet-rock collision"
-                    className="max-w-sm"
+                    className="w-full max-w-sm px-3"
                     />
                 </div>
             </div>
 
             <div className="flex flex-col justify-center items-center mt-8 p-4 border-t self-stretch">
-                <h2 className="text-xl text-accent">Pixcraft</h2>
+                <h2 className="text-xl text-accent py-4">Pixcraft</h2>
 
-                <div className="flex flex-row pt-8 items-center">
+                <div className="flex flex-col md:flex-row items-center">
                     <img src="/pixcraft/hi.jpg"
                     alt="Smiley face saying hi"
-                    className="max-w-sm px-3"
+                    className="w-full max-w-sm px-3"
                     />
                     <img src="/pixcraft/layering.jpg"
                     alt="Showing layering of canvas'"
-                    className="max-w-sm"
+                    className="w-full max-w-sm px-3"
                     />
-                    <p className="max-w-2xl px-4">
+                    <p className="w-full max-w-2xl px-4">
                         A 2D pixel art editor I built to make drawing pixel art for games convenient. Includes resizable canvas, multiple layers, drawing and shape/line tool, canvas transformation(flip, rotate), image import/export, undo functionality, color picker, and palette of previously used colors to pick from.
                         <br />
                         <span className="text-muted">Tech: C++, Raylib, Raygui, and CMake</span>
@@ -66,21 +66,21 @@ function Projects() {
             </div>
 
             <div className="flex flex-col justify-center items-center mt-8 p-4 border-t self-stretch">
-                <h2 className="text-xl text-accent">Tincan</h2>
+                <h2 className="text-xl text-accent py-4">Tincan</h2>
 
-                <div className="flex flex-row pt-8 items-center">
-                    <p className="max-w-2xl px-4">
+                <div className="flex flex-col md:flex-row items-center">
+                    <p className="w-full max-w-2xl px-4">
                         A real time chat app in the terminal. Includes multiple client support, multi-threaded server, chatrooms, colored text, and simple command protocol.
                         <br />
                         <span className="text-muted">Tech: Java 21, TCP Sockets</span>
                     </p>
                     <img src="/tincan/initialjoin.jpg"
                     alt="Multiple users join server"
-                    className="max-w-lg px-3"
+                    className="w-full max-w-lg px-3"
                     />
                     <img src="/tincan/chatworking.jpg"
                     alt="Two users chatting in the server"
-                    className="max-w-lg"
+                    className="w-full max-w-lg px-3"
                     />
                 </div>
             </div>
